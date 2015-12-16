679c54a664ab3c8565f6ee702e91b2391a59370b
patmallee <patmallee@gmail.com>
2015-12-15 20:09:52 -0500

    Add Comment model

--- micro-reddit/db/schema.rb
+++ micro-reddit/db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151216005547) do
+ActiveRecord::Schema.define(version: 20151216010915) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "message"
+    t.integer  "post_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
